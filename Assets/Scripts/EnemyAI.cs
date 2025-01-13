64ce04fdcfe371eded354b2be23c4135ed81131b
Polbar <polbarpolb@gmail.com>
2025-01-13 12:13:13 +0100

    Hecho

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,109 +1,136 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-
     public enum EnemyState
     {
         Patrolling,
-        Chasing, 
-
-        Searching
+        Chasing,
+        Searching,
+        Waiting,
+        Attacking
     }
 
     public EnemyState currentState;
 
     private NavMeshAgent _AIAgent;
-    private Transform _playerTranform;
+    private Transform _playerTransform;
 
-    //Puntos patrulla
+    // Puntos de patrulla
     [SerializeField] Transform[] _patrolPoints;
-    [SerializeField] Vector2 _patrolAreaSize = new Vector2(5, 5);
-    [SerializeField] Transform _patrolAreaCenter;
-    
-    //Cosas detencion
-    [SerializeField] float _visionRange = 20; 
+    private int _currentPatrolIndex = 0;
+
+    // Cosas de detección
+    [SerializeField] float _visionRange = 20;
     [SerializeField] float _visionAngle = 120;
     private Vector3 _playerLastPosition;
 
-    //Cosas busqueda
-
+    // Cosas de búsqueda
     float _searchTimer;
     float _searchWaitTime = 15;
     float _searchRadius = 10;
 
+    // Cosas de espera
+    float _waitTimer;
+    public float _waitTime = 5;
+
     void Awake()
     {
         _AIAgent = GetComponent<NavMeshAgent>();
-        _playerTranform = GameObject.FindWithTag("Player").transform;
-    } 
+        _playerTransform = GameObject.FindWithTag("Player").transform;
+    }
 
-    // Start is called before the first frame update
     void Start()
     {
         currentState = EnemyState.Patrolling;
-        SetRandomPatrolPoint();
+        SetNextPatrolPoint();
     }
 
-    // Update is called once per frame
     void Update()
     {
         switch (currentState)
         {
             case EnemyState.Patrolling:
                 Patrol();
-            break;
+                break;
             case EnemyState.Chasing:
                 Chase();
-            break;
+                break;
             case EnemyState.Searching:
                 Search();
-            break;
+                break;
+            case EnemyState.Waiting:
+                Wait();
+                break;
+            case EnemyState.Attacking:
+                Attack();
+                break;
         }
     }
 
     void Patrol()
     {
-        if(OnRange())
+        if (OnRange())
         {
             currentState = EnemyState.Chasing;
+            return;
         }
-        if(_AIAgent.remainingDistance < 0.5f)
+
+        if (_AIAgent.remainingDistance < 0.5f)
         {
-            SetRandomPatrolPoint();
+            currentState = EnemyState.Waiting;
+            _waitTimer = 0;
         }
-        
-    } 
+    }
+
+    void Wait()
+    {
+        _waitTimer += Time.deltaTime;
+
+        if (_waitTimer >= _waitTime)
+        {
+            currentState = EnemyState.Patrolling;
+            SetNextPatrolPoint();
+        }
+    }
 
     void Chase()
     {
-        if(!OnRange())
+        if (!OnRange())
         {
             currentState = EnemyState.Searching;
+            return;
         }
-        _AIAgent.destination = _playerTranform.position;
+
+        if (Vector3.Distance(transform.position, _playerTransform.position) < 2.0f)
+        {
+            currentState = EnemyState.Attacking;
+        }
+        else
+        {
+            _AIAgent.destination = _playerTransform.position;
+        }
     }
 
     void Search()
     {
-        if(OnRange())
+        if (OnRange())
         {
             currentState = EnemyState.Chasing;
+            return;
         }
 
         _searchTimer += Time.deltaTime;
 
-        if(_searchTimer < _searchWaitTime)
+        if (_searchTimer < _searchWaitTime)
         {
-            if(_AIAgent.remainingDistance < 0.5f)
+            if (_AIAgent.remainingDistance < 0.5f)
             {
                 Vector3 randomPoint;
-                if(RandomSearchPoint(_playerLastPosition, _searchRadius, out randomPoint))
+                if (RandomSearchPoint(_playerLastPosition, _searchRadius, out randomPoint))
                 {
                     _AIAgent.destination = randomPoint;
                 }
@@ -116,12 +143,18 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
+    void Attack()
+    {
+        Debug.Log("Enemy is attacking!");
+        currentState = EnemyState.Chasing;
+    }
+
     bool RandomSearchPoint(Vector3 center, float radius, out Vector3 point)
     {
         Vector3 randomPoint = center + Random.insideUnitSphere * radius;
 
         NavMeshHit hit;
-        if(NavMesh.SamplePosition(randomPoint, out hit, 4, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(randomPoint, out hit, 4, NavMesh.AllAreas))
         {
             point = hit.position;
             return true;
@@ -133,36 +166,33 @@ public class EnemyAI : MonoBehaviour
 
     bool OnRange()
     {
-
-        Vector3 directionToPlayer = _playerTranform.position - transform.position;
+        Vector3 directionToPlayer = _playerTransform.position - transform.position;
         float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
-        float distanceToPlayer = Vector3.Distance(transform.position, _playerTranform.position);
+        float distanceToPlayer = Vector3.Distance(transform.position, _playerTransform.position);
 
-        if(_playerTranform.position == _playerLastPosition)
+        if (_playerTransform.position == _playerLastPosition)
         {
             return true;
         }
 
-        if(distanceToPlayer > _visionRange)
+        if (distanceToPlayer > _visionRange)
         {
             return false;
         }
 
-        if(angleToPlayer > _visionAngle * 0.5f)
+        if (angleToPlayer > _visionAngle * 0.5f)
         {
             return false;
         }
 
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, directionToPlayer, out hit, distanceToPlayer))
+        if (Physics.Raycast(transform.position, directionToPlayer, out hit, distanceToPlayer))
         {
-            if(hit.collider.CompareTag("Player"))
+            if (hit.collider.CompareTag("Player"))
             {
-                _playerLastPosition = _playerTranform.position;
-
+                _playerLastPosition = _playerTransform.position;
                 return true;
             }
-
             else
             {
                 return false;
@@ -172,36 +202,16 @@ public class EnemyAI : MonoBehaviour
         return true;
     }
 
-    void SetRandomPatrolPoint()
+    void SetNextPatrolPoint()
     {
-        //_AIAgent.destination = _patrolPoints[Random.Range(0, _patrolPoints.Length)].position;
-        float RandomX = Random.Range(-_patrolAreaSize.x * 0.5f, _patrolAreaSize.x * 0.5f);
-        float RandomZ = Random.Range(-_patrolAreaSize.y * 0.5f, _patrolAreaSize.y * 0.5f);
-
-        Vector3 randomPoint = new Vector3(RandomX, 0, RandomZ) +_patrolAreaCenter.position;
-
-        _AIAgent.destination = randomPoint;
-    }
-
-    void OnDrawGizmos()
-    {
-        /*foreach (Transform point in _patrolPoints)
+        if (_patrolPoints.Length == 0)
         {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(point.position, 1);
-        }*/
+            return;
+        }
 
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(_patrolAreaCenter.position, new Vector3(_patrolAreaSize.x, 1, _patrolAreaSize.y));
+        _AIAgent.destination = _patrolPoints[_currentPatrolIndex].position;
 
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, _visionRange);
-
-        Gizmos.color = Color.yellow;
-        Vector3 fovLine1 = Quaternion.AngleAxis(_visionAngle * 0.5f, transform.up) * transform.forward * _visionRange;
-        Vector3 fovLine2 = Quaternion.AngleAxis(-_visionAngle * 0.5f, transform.up) * transform.forward * _visionRange;
-
-        Gizmos.DrawLine(transform.position, transform.position + fovLine1);
-        Gizmos.DrawLine(transform.position, transform.position + fovLine2);
+        // Incrementar el índice y reiniciarlo si alcanza el final
+        _currentPatrolIndex = (_currentPatrolIndex + 1) % _patrolPoints.Length;
     }
 }
